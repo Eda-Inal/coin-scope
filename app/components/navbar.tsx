d0146b92611e100074fac7b3c10ef7bfb2af7171
Eda Işıl İnal <edainalisil@gmail.com>
2025-02-04 17:45:32 +0300

    add css navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -11,8 +11,6 @@ import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../store'
 import { toggleTheme } from '../features/theme'
 
-
-
 const Navbar: React.FC = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -26,48 +24,52 @@ const Navbar: React.FC = () => {
     };
 
     return (
-        <nav className='py-3 px-2 sm:px-8 shadow-md dark:shadow-[0_2px_3px_0_rgba(255,255,255,0.2)]'>
-            <ul className='flex justify-between items-center '>
-                <div className='flex items-center gap-1 '>
-                    <Image src={Logo} width={32} height={32} alt='Logo' />
-                    <li>CryptoTrack </li>
+        <nav className="relative py-3 px-2 sm:px-8 shadow-md dark:shadow-[0_2px_3px_0_rgba(255,255,255,0.2)]">
+            <ul className="flex justify-between items-center">
+                <div className="flex items-center gap-1">
+                    <Image src={Logo} width={32} height={32} alt="Logo" />
+                    <li>CryptoTrack</li>
                 </div>
 
-                <div className='hidden sm:flex sm:gap-4 sm:items-center '>
+
+                <div className="hidden sm:flex sm:gap-4 sm:items-center">
                     <li><ChangeTheme /></li>
                     <li>Language</li>
-                    <li><button className='bg-blue-400 text-white px-3 py-1 rounded-md'>Log In </button></li>
-
-
+                    <li><button className="bg-blue-400 text-white px-3 py-1 rounded-md">Log In </button></li>
                 </div>
-                {
-                    isMenuOpen ?
-                        <CloseIcon
-                            className=" text-blue-500 sm:hidden cursor-pointer"
-                            onClick={handleHamburgerClick}
-                        /> :
-                        <SettingsIcon
-                            className=" text-blue-500 sm:hidden cursor-pointer"
-                            onClick={handleHamburgerClick}
-                        />
-                }
 
 
+                <div className="sm:hidden">
+                    {isMenuOpen ?
+                        <CloseIcon className="text-blue-500 cursor-pointer" onClick={handleHamburgerClick} />
+                        :
+                        <SettingsIcon className="text-blue-500 cursor-pointer" onClick={handleHamburgerClick} />
+                    }
+                </div>
             </ul>
-            <div className={`sm:hidden ${isMenuOpen ? 'block' : 'hidden'}  p-4 rounded-md mt-6 `}>
-                <ul className='flex flex-col gap-4 '>
-                    <li className='flex gap-2'> Theme
+
+
+            <div className={`sm:hidden ${isMenuOpen ? 'block' : 'hidden'} absolute top-0 left-0 w-full bg-lightBackground dark:bg-darkBackground shadow-md z-20 py-3 px-2`}>
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center gap-1">
+                        <Image src={Logo} width={32} height={32} alt="Logo" />
+                        <div>CryptoTrack</div>
+                    </div>
+                    <CloseIcon onClick={handleHamburgerClick} className='cursor-pointer text-blue-500' />
+                </div>
+                <ul className="flex flex-col gap-4 mt-8 items-center">
+                    <li className="flex gap-2">
+                        Theme
                         {mode === "dark" ? <LightIcon onClick={handleThemeChange} className="fill-current cursor-pointer" /> : <DarkIcon onClick={handleThemeChange} className="fill-current cursor-pointer" />}
                     </li>
                     <li>Language</li>
                     <li>
-                        <button className='bg-blue-400 text-white px-3 py-1 rounded-md'>Log In</button>
+                        <button className="bg-blue-400 text-white px-3 py-1 rounded-md">Log In</button>
                     </li>
                 </ul>
             </div>
-
         </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
