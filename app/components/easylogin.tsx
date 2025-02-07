fb83207a87eb677c89bf435b4ef3daef55a89d75
Eda Işıl İnal <edainalisil@gmail.com>
2025-02-07 08:58:00 +0300

    added a horizontal divider with "or" between login methods

--- app/components/easylogin.tsx
+++ app/components/easylogin.tsx
@@ -16,11 +16,17 @@ const EasyLogin: React.FC = () => {
                 <Image src="/google.png" alt="google icon" width={20} height={20} quality={80} />
                 <span>Continue with Google</span>
             </button>
-
+            <div className="relative flex items-center">
+                <div className="flex-grow border-t border-gray-300 dark:border-gray-600"></div>
+                <span className="px-2 text-gray-500 dark:text-gray-400 text-sm">or</span>
+                <div className="flex-grow border-t border-gray-300 dark:border-gray-600"></div>
+            </div>
             <button onClick={handleMailClick} className='w-72 dark:bg-darkSecondary bg-lightSecondary px-4 py-3 rounded-full flex items-center gap-6'>
                 <Image src="/mail.png" alt="google icon" width={20} height={20} quality={80} />
                 <span>Continue with Mail</span>
             </button>
+          
+
 
 
         </div>
