528d56c449b9f7db5ea6435a860a41d1fe9de6f6
Eda Işıl İnal <edainalisil@gmail.com>
2025-02-11 12:52:06 +0300

    add sign out

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -1,9 +1,23 @@
 'use client'
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { logOut } from '../features/userSlice';
+import { signOut } from 'firebase/auth';
+import { auth } from '@/firebaseConfig';
 
 
 const Profile: React.FC = () => {
+    const dispatch = useDispatch();
     const [isOpen, setIsOpen] = useState(false);
+
+    const handleSignOut = async () => {
+        try {
+            await signOut(auth);
+            dispatch(logOut());
+        } catch (error) {
+            console.error("Sign-out failed:", error);
+        }
+    };
     return (
         <div className="relative ">
             <button
@@ -16,16 +30,16 @@ const Profile: React.FC = () => {
             {isOpen && (
                 <ul className="absolute z-40  mt-1 w-28 flex flex-col items-start bg-lightBackground dark:bg-darkBackground   shadow-md rounded-sm left-1/2 transform -translate-x-1/2 text-sm ">
                     <li className="px-4 py-1 text-center cursor-pointer border-b hover:bg-lightSecondary  dark:hover:bg-darkSecondary gap-2  w-full"
-                       
+
                     >
                         Favourites
                     </li>
-                    <li className="px-4 py-1 text-center cursor-pointer hover:bg-lightSecondary   dark:hover:bg-darkSecondary gap-2  w-full"
-                      
+                    <li onClick={handleSignOut} className="px-4 py-1 text-center cursor-pointer hover:bg-lightSecondary   dark:hover:bg-darkSecondary gap-2  w-full"
+
                     >
-                      Sign out
+                        Sign out
                     </li>
-                
+
                 </ul>
             )}
         </div>
