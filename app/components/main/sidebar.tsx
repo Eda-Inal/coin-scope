c7001cf15cbd776b4e569b1d436da7fb4d7d399f
Eda Işıl İnal <edainalisil@gmail.com>
2025-02-13 10:28:30 +0300

    added design for the top 5 cryptocurrencies section

--- app/components/main/sidebar.tsx
+++ app/components/main/sidebar.tsx
@@ -5,22 +5,23 @@ import Favourites from './favs'
 const Sidebar: React.FC = () => {
     return (
         <div className="flex flex-col  h-full  ">
-            {/* small devices */}
-            <div className='w-full h-full bg-lightSecondary dark:bg-darkSecondary sm:hidden'>
+            {/* mobile, tablet */}
+            <div className='w-full h-full bg-lightSecondary dark:bg-darkSecondary lg:hidden'>
                 <div className='flex gap-2  px-2 '>
                     <button className='hover:text-primary'>Your Top</button>
                     <div>|</div>
                     <button className='hover:text-primary'>Favourites</button>
                 </div>
 
-                <div className="  h-full my-2 sm:hidden px-2 ">
+                <div className="  h-full my-2 lg:hidden px-2 ">
                     <Top />
                 </div>
             </div>
 
-            {/* sm-md */}
-            <div className='hidden w-full  sm:flex  h-full gap-3    '>
+            {/* big screens */}
+            <div className='hidden w-full  lg:flex  h-full gap-3    '>
                 <div className="  h-full w-1/2   rounded-md bg-lightSecondary dark:bg-darkSecondary px-2 ">
+                    <div>dfdf</div>
                     <Top />
                 </div>
                 <div className=" h-full  w-1/2 rounded-md bg-lightSecondary dark:bg-darkSecondary px-2  ">
--- app/components/main/top.tsx
+++ app/components/main/top.tsx
@@ -2,10 +2,50 @@ import React from 'react'
 
 
 const Top: React.FC = () => {
-  
+
     return (
         <>
-top5
+
+            <div className='flex flex-col gap-1   h-4/5 mt-2 w-full justify-between'>
+
+
+                <div className="flex flex-row justify-between font-semibold text-sm">
+                    <div className="flex-[0.5]   ">⭐</div>
+
+                    <span className="flex-[0.5] text-left ">#</span>
+                    <span className="flex-[1.2] text-left">Coin</span>
+                    <span className="flex-[1.2] text-left">Price</span>
+                    <span className="flex-[1.5] text-left">24s %</span>
+                    <span className="flex-[2] text-left sm:block hidden">Market Volume</span>
+                    <span className="flex-[2] text-left sm:block hidden">Market Cap</span>
+                </div>
+
+
+             
+                {Array(5)
+                    .fill(null)
+                    .map((_, index) => (
+                        <div key={index} className="flex flex-row justify-between items-center rounded-lg text-sm">
+                            <div className="flex-[0.5]  "> ⭐
+
+                            </div>
+
+                            <span className="flex-[0.5] text-left">{index + 1}</span>
+                            <span className="flex-[1.2] text-left">Coin</span>
+                            <span className="flex-[1.2] text-left">Price: -</span>
+                            <span className="flex-[1.5] text-left">24s %</span>
+                            <span className="flex-[2] text-left sm:block hidden">Market Volume -</span>
+                            <span className="flex-[2] text-left sm:block hidden ">Market Cap -</span>
+                        </div>
+
+                    ))}
+            </div>
+
+
+
+
+
+
         </>
 
     )
