1f36638ee1e4d6ed9fe445f5bb4de33567bd0505
Eda Işıl İnal <edainalisil@gmail.com>
2025-03-03 20:53:59 +0300

    add some css

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,43 +1,47 @@
 import React from 'react'
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { RootState } from '@/app/store'
+import { getTranslation } from '@/app/utils/getTranslation'
+import { useSelector } from 'react-redux'
 
 
 const Footer: React.FC = () => {
+  const locale = useSelector((state: RootState) => state.language.locale);
+  const t = getTranslation(locale);
+  return (
+    <>
 
-    return (
-        <>
+      <footer className="w-full  p-4 text-center mt-4">
+        <div className="flex justify-center space-x-6 mb-2">
 
-<footer className="w-full  p-4 text-center mt-4">
-      <div className="flex justify-center space-x-6 mb-2">
-      
-        <a
-          href="https://github.com/Eda-Inal"
-          target="_blank"
-          rel="noopener noreferrer"
-          className=" text-3xl  transition"
-        >
-          <FaGithub />
-        </a>
+          <a
+            href="https://github.com/Eda-Inal"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" text-3xl  transition"
+          >
+            <FaGithub />
+          </a>
 
-    
-        <a
-          href="https://www.linkedin.com/in/edainal"
-          target="_blank"
-          rel="noopener noreferrer"
-          className=" text-3xl  transition"
-        >
-          <FaLinkedin />
-        </a>
-      </div>
 
-   
-      <p className="text-sm text-gray-400">© {new Date().getFullYear()} All rights reserved.</p>
-    </footer>
+          <a
+            href="https://www.linkedin.com/in/edainal"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" text-3xl  transition"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
 
-        </>
 
-    )
+        <p className="text-sm text-gray-400">© {new Date().getFullYear()}{t.footer}</p>
+      </footer>
+
+    </>
+
+  )
 }
 
 export default Footer
--- app/components/main/coins.tsx
+++ app/components/main/coins.tsx
@@ -36,8 +36,8 @@ const Coins: React.FC = () => {
 
     return (
         <div className='flex flex-col gap-2 h-full w-full mt-2 justify-between'>
-            <div className="flex flex-row justify-between font-semibold p-2 border-b dark:border-b-gray-700">
-                <div className="flex-[0.3]">⭐</div>
+            <div className="flex flex-row justify-between font-semibold p-2 border-b dark:border-b-gray-700 items-center">
+                <div className="flex-[0.3]"></div>
                 <span className="flex-[0.3] text-left">#</span>
                 <span className="w-8 h-8 rounded-full mr-4  text-left"></span>
                 <span className="flex-[1.2] text-left ">{t.coin}</span>
